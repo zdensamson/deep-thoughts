1a1f407a1c7fe59c9649c6b44524d4cbdf4c8c10
Deku <zachdsamson@gmail.com>
2021-10-31 17:03:20 -0700

    User & Thought data-type-defs/resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,30 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World';
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+
+        thought: async(parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .poulate('friends')
+                .populate('thoughts');
         }
     }
 };
