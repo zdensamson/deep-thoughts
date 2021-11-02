2adfedc75a7f2f461c408e6cfbdf713656aaef76
Deku <zachdsamson@gmail.com>
2021-11-02 08:26:46 -0700

    started ThoughtList component build

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
+  // use the useQuery hook to make a query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
