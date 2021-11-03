595c85c8af82e1fa465abdfe17098fa8e6b3d1a6
Deku <zachdsamson@gmail.com>
2021-11-02 18:29:40 -0700

    partially through setting up CONCURRENTLY package

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
   // use the useQuery hook to make a query request
@@ -12,7 +13,13 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)"/>
+          )}
+        </div>
       </div>
     </main>
   );
