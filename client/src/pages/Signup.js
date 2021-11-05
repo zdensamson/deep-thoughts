29ce6488b1c69eb5494f5b09beac0c6d13ea8f6b
Deku <zachdsamson@gmail.com>
2021-11-05 14:37:28 -0700

    token stored in localStorage and used for every API request

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
+import Auth from '../utils/auth';
+
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
 
@@ -27,7 +29,8 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState }
       });
-      console.log(data);
+
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
